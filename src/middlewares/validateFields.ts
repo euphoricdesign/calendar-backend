90e4c9fb86c41bd588b190dc46c42d880dc42a6d
Merlina <noahchukita@gmail.com>
2024-07-21 21:07:09 -0300

    feat: Agregar validación para entradas de usuario

---
+++ src/middlewares/validateFields.ts
@@ -0,0 +1,12 @@
+import { validationResult } from "express-validator"
+import { Request, Response, NextFunction } from "express"
+
+const validateFields = (req: Request, res: Response, next: NextFunction) => {
+    const errors = validationResult(req)
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ ok: false, errors: errors.array() })
+    }
+    next()
+}
+
+export default validateFields
