4349ee95aa5a6297272dd0ed982c702ceff8a16f
Merlina <noahchukita@gmail.com>
2024-07-21 21:06:12 -0300

    chore: Configurar conexión a base de datos

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,14 @@
+import AppDataSource from "./config/data-source";
 import server from "./server";
+import "reflect-metadata"
 
 require('dotenv').config()
 
-server.listen(process.env.PORT, () => {
-    console.log(`Servidor escuchando en el puerto ${process.env.PORT}`)
-})
+AppDataSource.initialize().then(() => {
+    console.log("Conexión a la base de datos realizada con exito!");
+    
+    server.listen(process.env.PORT, () => {
+        console.log(`Servidor escuchando en el puerto ${process.env.PORT}`)
+    })
+}).catch((error) => console.log(error))
+
